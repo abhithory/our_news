8775229a417dfde1af4e5be8340174b5a4e855ec
Abhishek Thory <abhithory@gmail.com>
2021-09-27 16:17:44 +0530

    Updates

---
+++ precache-manifest.84bef0b50ef37a988369c8501f860895.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d875bc986bcc6cb09d61",
+    "url": "/our_news/static/js/main.d875bc98.chunk.js"
+  },
+  {
+    "revision": "282fd30747aab79ba925",
+    "url": "/our_news/static/css/1.d426c78b.chunk.css"
+  },
+  {
+    "revision": "282fd30747aab79ba925",
+    "url": "/our_news/static/js/1.282fd307.chunk.js"
+  },
+  {
+    "revision": "9a1c3650d6736019034b",
+    "url": "/our_news/static/js/runtime~main.9a1c3650.js"
+  },
+  {
+    "revision": "f4c8f6997096cc8f8cef4ad749d8721e",
+    "url": "/our_news/index.html"
+  }
+];
